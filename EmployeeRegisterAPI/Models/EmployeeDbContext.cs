6e537bb1439ff2847e8d3c1d254424ff144c2bad
Martin Lumban Gaol <martinlumbangaol880@gmail.com>
2022-10-03 21:10:56 +0700

    Add project files.

---
+++ EmployeeRegisterAPI/Models/EmployeeDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeRegisterAPI.Models
+{
+    public class EmployeeDbContext : DbContext
+    {
+        public EmployeeDbContext(DbContextOptions<EmployeeDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<EmployeeModel> Employees { get; set; }
+    }
+}
